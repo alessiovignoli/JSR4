8accfae0d0fdd99255745c94a4d8981b81e9e1fd
Alessio Vignoli <alessio.vignoli3@studio.unibo.it>
2023-06-09 19:20:19 +0200

    fucked up on a revert

--- bin/match_table_louncher.py
+++ bin/match_table_louncher.py
@@ -11,11 +11,11 @@ def get_args():
     parser.add_argument("-f1", "--input1", type=str, required=True, metavar="FILE", help='first input file, the file has to have a unique spatiator/delimiter never found in the field separated by it, all line in this file must abide to this rule. At least the column of the key used for matching should always have the same position in every line.')
     parser.add_argument("-f2", "--input2", type=str, required=True, metavar="FILE", help='the above rules have to apply to this file as well')
     parser.add_argument("-o", "--output", type=str, required=True, metavar="FILE", help='the outputfilename or path')
-    parser.add_argument("-p1", "--pos1", type=int, required=False, nargs='?', const=1, default=0, metavar="field/column position/index", help='default 0, integer pointing to the column/field to be used as a key in file1, WARNING the notation must be python based, meaning first field = 0')
-    parser.add_argument("-p2", "--pos2", type=int, required=False, nargs='?', const=1, default=0, metavar="field/column position/index", help='same rule as above but this applies to file2 instead.')
-    parser.add_argument("-d1", "--delimiter1", type=str, required=False, nargs='?', const=1, default='\t', metavar="delimiter/spatiator", help='default <tab>, with this flag the spatiator/delimiter of the file can be specified, it can be whatever string, for special charachters use either \' or \" to encapsulate them, they have to be pythonic simbols.')
-    parser.add_argument("-d2", "--delimiter2", type=str, required=False, nargs='?', const=1, default='\t', metavar="delimiter/spatiator", help='all rules as above apply, it can be different from the value given to the above flag, in that case the output file will be delimited using the above flag value.')
-    parser.add_argument("--check_types", type=bool, required=False, nargs='?', const=1, default=False, metavar="debug", help='default False, no type of the variables will be checked at line level inside the script, this flag is usefull for debugging.')
+    parser.add_argument("-p1", "--pos1", type=int, required=False,  nargs='?', default=0, metavar="field/column position/index", help='default 0, integer pointing to the column/field to be used as a key in file1, WARNING the notation must be python based, meaning first field = 0')
+    parser.add_argument("-p2", "--pos2", type=int, required=False,  nargs='?', default=0, metavar="field/column position/index", help='same rule as above but this applies to file2 instead.')
+    parser.add_argument("-d1", "--delimiter1", type=str, required=False,  nargs='?', const='\t', default='\t', metavar="delimiter/spatiator", help='default <tab>, with this flag the spatiator/delimiter of the file can be specified, it can be whatever string, for special charachters use either \' or \" to encapsulate them, they have to be pythonic simbols.')
+    parser.add_argument("-d2", "--delimiter2", type=str, required=False,  nargs='?', const='\t', default='\t', metavar="delimiter/spatiator", help='all rules as above apply, it can be different from the value given to the above flag, in that case the output file will be delimited using the above flag value.')
+    parser.add_argument("--check_types", required=False, nargs='?', default=False, metavar="debug", help='default False, no type of the variables will be checked at line level inside the script, this flag is usefull for debugging.')
 
     args = parser.parse_args()
     return args
@@ -37,4 +37,9 @@ def main(file_1, file_2, output, pos1=0, pos2=0, del1='\t', del2='\t', check=Fal
 
 if __name__ == "__main__":
     args = get_args()
+
+    # To handle complications from command line string arguments
+    if args.check_types == 'false' or args.check_types == 'False':
+        args.check_types = False
+
     main(args.input1, args.input2, args.output, args.pos1, args.pos2, args.delimiter1, args.delimiter2, args.check_types)
