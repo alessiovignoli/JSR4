b7f8f98db7271b934d2fc468fb09938d4a33107c
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2023-07-07 17:46:05 +0200

    adapting to the changes in the version of python codebase

--- bin/match_table_louncher.py
+++ bin/match_table_louncher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from python_codebase.tabular_matcher import MatchByKeyPos
+from python_codebase.tabular import TabularFile
 
 def get_args():
 
@@ -16,22 +16,21 @@ def get_args():
     parser.add_argument("-d1", "--delimiter1", type=str, required=False,  nargs='?', const='\t', default='\t', metavar="delimiter/spatiator", help='default <tab>, with this flag the spatiator/delimiter of the file can be specified, it can be whatever string, for special charachters use either \' or \" to encapsulate them, they have to be pythonic simbols.')
     parser.add_argument("-d2", "--delimiter2", type=str, required=False,  nargs='?', const='\t', default='\t', metavar="delimiter/spatiator", help='all rules as above apply, it can be different from the value given to the above flag, in that case the output file will be delimited using the above flag value.')
     parser.add_argument("--check_types", required=False, nargs='?', default=False, metavar="debug", help='default False, no type of the variables will be checked at line level inside the script, this flag is usefull for debugging.')
+    parser.add_argument("--compress", required=False, nargs='?', default=False, metavar="FLAG", help='tells the script to compress using gzip the output resulting table. Default False, do not compress.')
 
     args = parser.parse_args()
     return args
 
 
-def main(file_1, file_2, output, pos1=0, pos2=0, del1='\t', del2='\t', check=False):
+def main(file_1, file_2, output, pos_1=0, pos_2=0, del1='\t', del2='\t', do_compress=False, check=False):
 
-    # Open the files and read them line by line
-    with open(file_1, "r") as file1, open(output, 'w') as out:
-        for line1 in file1:
-            with open(file_2, "r") as file2:
-                for line2 in file2:
-                    matched_lines = MatchByKeyPos(line1, line2, pos1, pos2, del1, del2, check)
-                    matched_line = matched_lines.Match()
-                    if matched_line:
-                        out.write(matched_line)
+    # Make every file a instance of the correct class
+    file1_obj = TabularFile(file_1, del1)
+    file_2_obj = TabularFile(file_2, del2)
+    output_obj = TabularFile(output)
+
+    # Call the function that will do the rest
+    file1_obj.IntersectTables(file_2_obj, output_obj, pos1=pos_1, pos2=pos_2, compress=do_compress, check_type=check)
 
 
 
@@ -41,5 +40,7 @@ if __name__ == "__main__":
     # To handle complications from command line string arguments
     if args.check_types == 'false' or args.check_types == 'False':
         args.check_types = False
+    if args.compress == 'false' or args.compress == 'False':
+        args.compress = False
 
-    main(args.input1, args.input2, args.output, args.pos1, args.pos2, args.delimiter1, args.delimiter2, args.check_types)
+    main(args.input1, args.input2, args.output, args.pos1, args.pos2, args.delimiter1, args.delimiter2, args.compress, args.check_types)
--- modules/tabular_matcher.nf
+++ modules/tabular_matcher.nf
@@ -8,11 +8,19 @@ process table_matcher {
 	tuple path(infile1), path(infile2), val(outname)
 
 	output:
-        path outname, emit: matched_table
+        path "${outname}*", emit: matched_table
         stdout emit: standardout                //for debug
 
 	script:
 	"""
-	match_table_louncher.py -f1 ${infile1} -f2 ${infile2} -o ${outname} -p1 ${params.pos1_TAB} -p2 ${params.pos2_TAB} -d1 ${params.del1_TAB} -d2 ${params.del2_TAB} --check_types ${params.check_TAB}
+	match_table_louncher.py -f1 ${infile1} \
+							-f2 ${infile2} \
+							-o ${outname} \
+							-p1 ${params.pos1_TAB} \
+							-p2 ${params.pos2_TAB} \
+							-d1 ${params.del1_TAB} \
+							-d2 ${params.del2_TAB} \
+							--check_types ${params.check_TAB} \
+							--compress ${params.compress_TAB}
 	"""
 }
